70e8b72f3cf6a6cad6f3ebaf3e79b6efb0dc81d5
Gabrielllloo <gabrielvicenteorallo@gmail.com>
2025-04-07 14:16:47 -0400

    1

--- Untitled-1.py
+++ Untitled-1.py
@@ -19,4 +19,29 @@ def play_game(rounds=10)
     """
     player_score = 0
     robot_score = 0
-    
+
+    for i in range(1, range+1):
+        print(f"Round {i}:")
+        # Player Turn
+        p_dice1, p_dice2, p_tota1 = roll_dice()
+        print(f"The dice rolled by the player: {p_dice1} and {p_dice2} -> Score: {p_tota1}")
+        
+        # Robot Round
+        r_dice1, r_dice2, r_tota1 = roll_dice()
+        print(f"The dice rolled by the robot: {r_dice1} and {r_dice2} -> Score: {r_tota1}")
+
+        if p_tota1 > r_tota1:
+            player_score += 1
+            print("The player wins the round! \n")
+        elif p_tota1 < r_tota1:
+            robot_score += 1
+            print("The robot wins the round! \n")
+        else:
+            print("Game draw! \n")
+
+if __name__ == '__main__':
+    while True:
+        play_game()
+        choice = input("Another round? (y/n): ").strip().lower()
+        if choice != 'y':
+            break
